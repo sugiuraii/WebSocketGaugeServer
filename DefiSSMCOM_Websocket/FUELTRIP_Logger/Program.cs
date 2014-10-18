1859360b562700f96a39103092d1560ac522bc62
Sugiura K Nigo <sugiura.k.nigo@gmail.com>
2014-10-18 05:26:49 +0000

    20140504 Version : Delete Setting Designer. Add defiserver_settings.xml,ssmserver_settings.xml, fueltriplogger_settings.xml
    git-svn-id: file:///c/Users/kuniaki/Repository/DefiSSMCOM_Websocket/trunk@21 ea906476-c518-7947-be9f-b151201fe8af

--- DefiSSMCOM_Websocket/FUELTRIP_Logger/Program.cs
+++ DefiSSMCOM_Websocket/FUELTRIP_Logger/Program.cs
@@ -1,18 +1,56 @@
 ﻿using System;
+using System.Xml;
+using System.IO;
 using log4net;
 
 namespace FUELTRIP_Logger
 {
+    public class AppSettings
+    {
+        public string defiserver_url;
+        public string ssmserver_url;
+        public int websocket_port;
+    }
+
 	class MainClass
 	{
 
         //log4net
         private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
+        private static AppSettings appsetting;
 		public static void Main (string[] args)
 		{
-			FUELTRIP_Logger fueltriplogger1 = new FUELTRIP_Logger (Properties.Settings.Default.DefiCOM_Websocket_URL,Properties.Settings.Default.SSMCOM_WebSocket_URL);
-            fueltriplogger1.WebsocketServer_ListenPortNo = Properties.Settings.Default.Listen_Port;
+            try
+            {
+                load_setting_xml("fueltriplogger_settings.xml");
+            }
+            catch (XmlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+
+			FUELTRIP_Logger fueltriplogger1 = new FUELTRIP_Logger (appsetting.defiserver_url,appsetting.ssmserver_url);
+            fueltriplogger1.WebsocketServer_ListenPortNo = appsetting.websocket_port;
 
 			Console.WriteLine("The server started successfully, press key 'q' to stop it!");
 
@@ -26,5 +64,33 @@ namespace FUELTRIP_Logger
 
 			fueltriplogger1.stop ();
 		}
+
+        private static void load_setting_xml(string filepath)
+        {
+            //XmlSerializerオブジェクトの作成
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(AppSettings));
+
+            //ファイルを開く
+            System.IO.FileStream fs =
+                new System.IO.FileStream(filepath, System.IO.FileMode.Open);
+
+            try
+            {
+                //XMLファイルから読み込み、逆シリアル化する
+                appsetting =
+                    (AppSettings)serializer.Deserialize(fs);
+
+            }
+            catch (XmlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
 	}
 }
--- DefiSSMCOM_Websocket/SSMCOM_WebSocket_Server/Program.cs
+++ DefiSSMCOM_Websocket/SSMCOM_WebSocket_Server/Program.cs
@@ -3,19 +3,57 @@ using System.Threading;
 using System.Collections;
 using DefiSSMCOM.WebSocket;
 using log4net;
+using System.Xml;
+using System.IO;
 
 namespace SSMCOM_WebSocket_Server
 {
+    public class AppSettings
+    {
+        public string comport;
+        public int websocket_port;
+    }
 	class MainClass
 	{
 		static private SSMCOM_Websocket ssmcomserver1;
         private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static AppSettings appsetting;
 
 		public static void Main (string[] args)
 		{
 			ssmcomserver1 = new SSMCOM_Websocket ();
-            ssmcomserver1.SSMCOM_PortName = Properties.Settings.Default.SSMCOM_PortName;
-            ssmcomserver1.Websocket_PortNo = Properties.Settings.Default.Websocket_Port;
+
+            try
+            {
+                load_setting_xml("ssmserver_settings.xml");
+            }
+            catch (XmlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger.Error(ex.Message);
+                return;
+            }
+
+            ssmcomserver1.SSMCOM_PortName = appsetting.comport;
+            ssmcomserver1.Websocket_PortNo = appsetting.websocket_port;
 			
             Console.WriteLine("The server started successfully, press key 'q' to stop it!");
 
@@ -29,6 +67,33 @@ namespace SSMCOM_WebSocket_Server
 
 			ssmcomserver1.stop ();
 		}
+
+        private static void load_setting_xml(string filepath)
+        {
+            //XmlSerializerオブジェクトの作成
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(AppSettings));
+
+            //ファイルを開く
+            System.IO.FileStream fs =
+                new System.IO.FileStream(filepath, System.IO.FileMode.Open);
+
+            try
+            {
+                //XMLファイルから読み込み、逆シリアル化する
+                appsetting =
+                    (AppSettings)serializer.Deserialize(fs);
+
+            }
+            catch (XmlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
 
 	}
 }
