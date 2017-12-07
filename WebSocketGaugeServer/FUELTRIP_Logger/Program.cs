6853b26c372e14f3ff9a70ff2eeb6273b59bee84
sz2 <sugiura.k.nigo@gmail.com>
2017-12-08 00:02:02 +0900

    Add arduinoserver_url, elm327server_url on fueltriplogger_settings.xml.

--- WebSocketGaugeServer/FUELTRIP_Logger/Program.cs
+++ WebSocketGaugeServer/FUELTRIP_Logger/Program.cs
@@ -10,6 +10,8 @@ namespace FUELTRIP_Logger
     {
         public string defiserver_url;
         public string ssmserver_url;
+        public string arduinoserver_url;
+        public string elm327server_url;
         public int websocket_port;
         public int keepalive_interval;
 
