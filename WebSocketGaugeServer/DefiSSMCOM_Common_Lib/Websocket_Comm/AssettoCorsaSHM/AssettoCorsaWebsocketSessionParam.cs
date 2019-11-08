8628fef654ef2f37195e32dc0cf0f17e9fd957ce
k_sz2 <sugiura.k.nigo@gmail.com>
2019-11-08 11:17:38 +0900

    Add AssettoCorsaSHM/AssettoCorsaWebsocketSessionParam.cs.

---
+++ WebSocketGaugeServer/DefiSSMCOM_Common_Lib/Websocket_Comm/AssettoCorsaSHM/AssettoCorsaWebsocketSessionParam.cs
@@ -0,0 +1,36 @@
+﻿using DefiSSMCOM_Common_Lib.AssetoCorsaSHM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiSSMCOM_Common_Lib.Websocket_Comm.AssettoCorsaSHM
+{
+    /// <summary>
+    /// Property data for AssettoCorsaWebsocket sessions.
+    /// </summary>
+    public class AssettoCorsaWebsocketSessionParam
+    {
+        public readonly Dictionary<AssettoCorsaSHMPhysicsParameterCode, bool> PhysicsDataSendList;
+        public readonly Dictionary<AssettoCorsaSHMGraphicsParameterCode, bool> GraphicsDataSendList;
+        public readonly Dictionary<AssettoCorsaSHMStaticInfoParameterCode, bool> StaticInfoDataSendList;
+
+        public uint PhysicsDataSendInterval = 0;
+        public uint GraphicsDataSendInterval = 0;
+        public uint StaticInfoDataSendInterval = 0;
+
+        public AssettoCorsaWebsocketSessionParam()
+        {
+            PhysicsDataSendList = new Dictionary<AssettoCorsaSHMPhysicsParameterCode, bool>();
+            GraphicsDataSendList = new Dictionary<AssettoCorsaSHMGraphicsParameterCode, bool>();
+            StaticInfoDataSendList = new Dictionary<AssettoCorsaSHMStaticInfoParameterCode, bool>();
+            foreach (AssettoCorsaSHMPhysicsParameterCode c in Enum.GetValues(typeof(AssettoCorsaSHMPhysicsParameterCode)))
+                PhysicsDataSendList.Add(c, false);
+            foreach (AssettoCorsaSHMGraphicsParameterCode c in Enum.GetValues(typeof(AssettoCorsaSHMGraphicsParameterCode)))
+                GraphicsDataSendList.Add(c, false);
+            foreach (AssettoCorsaSHMStaticInfoParameterCode c in Enum.GetValues(typeof(AssettoCorsaSHMStaticInfoParameterCode)))
+                StaticInfoDataSendList.Add(c, false);
+        }
+    }
+}
