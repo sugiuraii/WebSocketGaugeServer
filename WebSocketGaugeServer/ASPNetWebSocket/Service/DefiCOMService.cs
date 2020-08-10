4ead1dbb8a47cd41b5cdb2842289931485fcb7f5
sz2 <sugiura.k.nigo@gmail.com>
2020-08-11 00:31:38 +0900

    Add test project.

---
+++ WebSocketGaugeServer/ASPNetWebSocket/Service/DefiCOMService.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using DefiSSMCOM.Defi;
+using DefiSSMCOM.WebSocket;
+using DefiSSMCOM.WebSocket.JSON;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ASPNetWebSocket.Service
+{
+    public class DefiCOMService
+    {
+        private readonly DefiCOM defiCOM;
+        private readonly Dictionary<Guid, WebSocket> WebSockets = new Dictionary<Guid, WebSocket>();
+        private readonly Dictionary<Guid, DefiCOMWebsocketSessionParam> SessionParams = new Dictionary<Guid, DefiCOMWebsocketSessionParam>();
+
+        public void AddWebSocket(Guid sessionGuid, WebSocket websocket)
+        {
+            this.WebSockets.Add(sessionGuid, websocket);
+            this.SessionParams.Add(sessionGuid, new DefiCOMWebsocketSessionParam());
+        }
+
+        public void RemoveWebSocket(Guid sessionGuid)
+        {
+            this.WebSockets.Remove(sessionGuid);
+            this.SessionParams.Remove(sessionGuid);
+        }
+
+        public DefiCOMWebsocketSessionParam GetSessionParam(Guid guid) 
+        {
+            return this.SessionParams[guid];
+        }
+
+        public DefiCOM DefiCOM { get { return defiCOM; } }
+        public DefiCOMService(string comportName)
+        {
+            this.defiCOM = new DefiCOM();
+            this.defiCOM.PortName = comportName;
+
+            // Register websocket broad cast
+            this.defiCOM.DefiPacketReceived += async (sender, args) =>
+            {
+                foreach (var session in WebSockets)
+                {
+                    var guid = session.Key;
+                    var websocket = session.Value;
+                    var sessionparam = GetSessionParam(guid);
+
+                    var msg_data = new ValueJSONFormat();        
+                    if (sessionparam.SendCount < sessionparam.SendInterval)
+                        sessionparam.SendCount++;
+                    else
+                    {
+                        foreach (DefiParameterCode deficode in Enum.GetValues(typeof(DefiParameterCode)))
+                        {
+                            if (sessionparam.Sendlist[deficode])
+                                msg_data.val.Add(deficode.ToString(), defiCOM.get_value(deficode).ToString());
+                        }
+
+                        if (msg_data.val.Count > 0)
+                        {
+                            string msg = JsonConvert.SerializeObject(msg_data);
+                            byte[] buf = Encoding.UTF8.GetBytes(msg);
+                            await websocket.SendAsync(new ArraySegment<byte>(buf), WebSocketMessageType.Text, true, CancellationToken.None);
+                        }
+                        sessionparam.SendCount = 0;
+                    }
+                }
+            };
+        }
+    }
+}
--- WebSocketGaugeServer/ASPNetWebSocket/Startup.cs
+++ WebSocketGaugeServer/ASPNetWebSocket/Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ASPNetWebSocket.Service;
+using System.Net.WebSockets;
+using System.Threading;
+using DefiSSMCOM.WebSocket;
+using DefiSSMCOM.WebSocket.JSON;
 
 namespace ASPNetWebSocket
 {
@@ -16,6 +21,7 @@ namespace ASPNetWebSocket
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<DefiCOMService>(new DefiCOMService("COM3"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,22 +32,94 @@ namespace ASPNetWebSocket
                 app.UseDeveloperExceptionPage();
             }
 
-            var webSocketOptions = new WebSocketOptions() 
+            var webSocketOptions = new WebSocketOptions()
             {
                 KeepAliveInterval = TimeSpan.FromSeconds(120),
                 ReceiveBufferSize = 4 * 1024
             };
             app.UseWebSockets(webSocketOptions);
-            
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
+                if (context.WebSockets.IsWebSocketRequest)
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    await Handle(context, webSocket);
+                }
+                else
+                {
+                    await next();
+                }
             });
+        }
+
+        private async Task Handle(HttpContext context, WebSocket webSocket)
+        {
+            var service = (DefiCOMService)context.RequestServices.GetRequiredService(typeof(DefiCOMService));
+
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            var connectionID = Guid.NewGuid();
+            service.AddWebSocket(connectionID, webSocket);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            service.RemoveWebSocket(connectionID);
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            await tickTask;
+        }
+
+        private void processReceivedJSONMessage(string receivedJSONmode, string message, DefiCOMWebsocketSessionParam sessionParam, WebSocket ws)
+        {
+            switch (receivedJSONmode)
+            {
+                case (ResetJSONFormat.ModeCode):
+                    sessionParam.reset();
+                    send_response_msg(session, "Defi Websocket all parameter reset.");
+                    break;
+                case (DefiWSSendJSONFormat.ModeCode):
+                    DefiWSSendJSONFormat msg_obj_wssend = JsonConvert.DeserializeObject<DefiWSSendJSONFormat>(message);
+                    msg_obj_wssend.Validate();
+                    sessionParam.Sendlist[(DefiParameterCode)Enum.Parse(typeof(DefiParameterCode), msg_obj_wssend.code)] = msg_obj_wssend.flag;
+
+                    send_response_msg(session, "Defi Websocket send_flag for : " + msg_obj_wssend.code.ToString() + " set to : " + msg_obj_wssend.flag.ToString());
+                    break;
+
+                case (DefiWSIntervalJSONFormat.ModeCode):
+                    DefiWSIntervalJSONFormat msg_obj_interval = JsonConvert.DeserializeObject<DefiWSIntervalJSONFormat>(message);
+                    msg_obj_interval.Validate();
+                    sessionparam.SendInterval = msg_obj_interval.interval;
+
+                    send_response_msg(session, "Defi Websocket send_interval to : " + msg_obj_interval.interval.ToString());
+                    break;
+                default:
+                    throw new JSONFormatsException("Unsuppoted mode property.");
+            }
+        }
+
+        protected void send_error_msg(WebSocket ws, string message)
+        {
+            ErrorJSONFormat json_error_msg = new ErrorJSONFormat();
+            json_error_msg.msg = message;
+            
+            session.Send(json_error_msg.Serialize());
+            IPAddress destinationAddress = session.RemoteEndPoint.Address;
+            logger.Error("Send Error message to " + destinationAddress.ToString() + " : " + message);
+        }
+
+        protected void send_response_msg(WebSocket Ws, string message)
+        {
+            ResponseJSONFormat json_response_msg = new ResponseJSONFormat();
+            json_response_msg.msg = message;
+            session.Send(json_response_msg.Serialize());
+
+            IPAddress destinationAddress = session.RemoteEndPoint.Address;
+            logger.Info("Send Response message to " + destinationAddress.ToString() + " : " + message);
         }
     }
 }
