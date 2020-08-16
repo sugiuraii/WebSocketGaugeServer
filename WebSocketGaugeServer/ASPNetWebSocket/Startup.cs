6a32dfe2a3e4b1d9b9c23112a22f23699b9ed970
sz2 <sugiura.k.nigo@gmail.com>
2020-08-17 05:31:02 +0900

    Change default comport name.

--- WebSocketGaugeServer/ASPNetWebSocket/Startup.cs
+++ WebSocketGaugeServer/ASPNetWebSocket/Startup.cs
@@ -28,7 +28,7 @@ namespace ASPNetWebSocket
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<DefiCOMService>(_ => new DefiCOMService("COM3"));
+            services.AddSingleton<DefiCOMService>(_ => new DefiCOMService("/dev/ttyUSB1"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
