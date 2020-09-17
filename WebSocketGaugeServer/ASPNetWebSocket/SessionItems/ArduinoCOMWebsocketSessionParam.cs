746ad506c1d076792121edf80c7daf8e76b85387
sz2 <sugiura.k.nigo@gmail.com>
2020-09-17 22:55:25 +0900

    Add ArduinoCOMWebsocketSessionParam.

---
+++ WebSocketGaugeServer/ASPNetWebSocket/SessionItems/ArduinoCOMWebsocketSessionParam.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using DefiSSMCOM.Arduino;
+
+namespace DefiSSMCOM.WebSocket
+{
+    public class ArduinoCOMWebsocketSessionParam
+    {
+        /// <summary>
+        /// Parameter code list to communicate in websocket.
+        /// </summary>
+        public Dictionary<ArduinoParameterCode, bool> Sendlist;
+        /// <summary>
+        /// Interval to send websocket messsage.
+        /// </summary>
+        public int SendInterval { get; set; }
+        public int SendCount { get; set; }
+
+        public ArduinoCOMWebsocketSessionParam()
+        {
+            this.Sendlist = new Dictionary<ArduinoParameterCode, bool>();
+
+            foreach (ArduinoParameterCode code in Enum.GetValues(typeof(ArduinoParameterCode)))
+            {
+                this.Sendlist.Add(code, false);
+            }
+
+            this.SendInterval = 0;
+            this.SendCount = 0;
+        }
+
+        public void reset()
+        {
+            foreach (ArduinoParameterCode code in Enum.GetValues(typeof(ArduinoParameterCode)))
+            {
+                this.Sendlist[code] = false;
+            }
+
+            this.SendInterval = 0;
+            this.SendCount = 0;
+        }
+    }
+}
