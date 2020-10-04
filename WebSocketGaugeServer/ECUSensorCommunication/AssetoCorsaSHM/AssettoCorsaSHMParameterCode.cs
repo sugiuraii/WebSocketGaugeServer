fd6113ea87e701df99751e1b2e268fe2088aa64b
sz2 <sugiura.k.nigo@gmail.com>
2020-10-04 23:46:49 +0900

    Change namespace of AssettoCorsaSHM.

--- WebSocketGaugeServer/ECUSensorCommunication/AssetoCorsaSHM/AssettoCorsaSHMParameterCode.cs
+++ WebSocketGaugeServer/ECUSensorCommunication/AssetoCorsaSHM/AssettoCorsaSHMParameterCode.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DefiSSMCOM.AssetoCorsaSHM
+namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.AssettoCorsaSHM
 {
     public enum AssettoCorsaSHMPhysicsParameterCode
     {
