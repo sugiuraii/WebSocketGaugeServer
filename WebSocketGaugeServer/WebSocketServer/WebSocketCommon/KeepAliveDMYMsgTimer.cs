24e8d68678a0c9cd40a5fbd0b4b31393cb4ec969
sz2 <sugiura.k.nigo@gmail.com>
2020-10-05 23:14:50 +0900

    Move KeepAliveDMYMsgTimer to WebSocketCommon.

--- WebSocketGaugeServer/WebSocketServer/WebSocketCommon/KeepAliveDMYMsgTimer.cs
+++ WebSocketGaugeServer/WebSocketServer/WebSocketCommon/KeepAliveDMYMsgTimer.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Net.WebSockets;
 
-namespace DefiSSMCOM.WebSocket
+namespace SZ2.WebSocketGaugeServer.WebSocketServer.WebSocketCommon
 {
     using WebSocket = System.Net.WebSockets.WebSocket;
     public class KeepAliveDMYMsgTimer
