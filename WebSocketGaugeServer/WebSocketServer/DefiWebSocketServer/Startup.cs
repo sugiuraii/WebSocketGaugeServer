221eb8ce82b90c6c34ebab48e55ab444b1fb3cab
sz2 <sugiura.k.nigo@gmail.com>
2021-01-25 00:30:33 +0900

    Implement static files to DefiWebSocketServer.

--- WebSocketGaugeServer/WebSocketServer/DefiWebSocketServer/Startup.cs
+++ WebSocketGaugeServer/WebSocketServer/DefiWebSocketServer/Startup.cs
@@ -47,7 +47,8 @@ namespace SZ2.WebSocketGaugeServer.WebSocketServer.DefiWebSocketServer
             app.UseWebSockets(webSocketOptions);
 
             app.UseRouting();
-            
+            app.UseDefaultFiles ();
+            app.UseStaticFiles();
             app.Use(async (context, next) =>
             {
                 if (context.WebSockets.IsWebSocketRequest)
