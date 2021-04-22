b07b0fa94419ca052bff87c4ffa6659ce9f53015
sz2 <sugiura.k.nigo@gmail.com>
2021-04-22 22:27:00 +0900

    Separate middleware.

--- WebSocketGaugeServer/WebSocketServer/WebSocketCommon/Middleware/IWebSocketHandleMiddleware.cs
+++ WebSocketGaugeServer/WebSocketServer/WebSocketCommon/Middleware/IWebSocketHandleMiddleware.cs
@@ -1,13 +1,12 @@
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 
 namespace SZ2.WebSocketGaugeServer.WebSocketServer.WebSocketCommon.Middleware
 {
     public interface IWebSocketHandleMiddleware
     {
-        Task HandleHttpConnection(HttpContext context, WebSocket webSocket, ILogger logger, CancellationToken ct);
+        Task HandleHttpConnection(HttpContext context, WebSocket webSocket, CancellationToken ct);
     }
 }
