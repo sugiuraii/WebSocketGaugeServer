974bcc0ab62e26beec73fe3fc04af9f911b9cb6a
sz2 <sugiura.k.nigo@gmail.com>
2021-05-07 00:30:50 +0900

    Add blazor service injection.

--- WebSocketGaugeServer/WebSocketDataLogger/FUELTRIPLogger/Startup.cs
+++ WebSocketGaugeServer/WebSocketDataLogger/FUELTRIPLogger/Startup.cs
@@ -16,6 +16,9 @@ namespace SZ2.WebSocketGaugeServer.WebSocketDataLogger.FUELTRIPLogger
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
             services.AddSingleton<FUELTRIPService>();
             services.AddTransient<MemoryLoggerModel>();
             services.AddTransient<ServiceConfigurationModel>();
