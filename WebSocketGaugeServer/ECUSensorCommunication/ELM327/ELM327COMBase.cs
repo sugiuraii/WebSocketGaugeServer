6a88c973d65ab094c723e244cbd5236272d9f21d
sz2 <sugiura.k.nigo@gmail.com>
2021-05-14 23:40:03 +0900

    Remove unused using.

--- WebSocketGaugeServer/ECUSensorCommunication/ELM327/ELM327COMBase.cs
+++ WebSocketGaugeServer/ECUSensorCommunication/ELM327/ELM327COMBase.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using Microsoft.Extensions.Logging;
-using System.Text.RegularExpressions;
 
 namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.ELM327
 {
