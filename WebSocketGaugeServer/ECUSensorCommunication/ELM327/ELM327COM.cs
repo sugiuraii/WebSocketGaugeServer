b84828f7ac8192a28ac90d47ba6a65026bc67f9f
sz2 <sugiura.k.nigo@gmail.com>
2021-05-14 23:30:21 +0900

    Separate ELM327DataReceivedEventArgs

--- WebSocketGaugeServer/ECUSensorCommunication/ELM327/ELM327COM.cs
+++ WebSocketGaugeServer/ECUSensorCommunication/ELM327/ELM327COM.cs
@@ -387,11 +387,4 @@ namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.ELM327
         }
 
     }
-
-
-    public class ELM327DataReceivedEventArgs : EventArgs
-    {
-        public bool Slow_read_flag { get; set; }
-        public List<OBDIIParameterCode> Received_Parameter_Code { get; set; }
-    }
 }
