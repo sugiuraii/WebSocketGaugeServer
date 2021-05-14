51a5b9b0bb44598546e35e7a09f294eb7934146a
sz2 <sugiura.k.nigo@gmail.com>
2021-05-14 23:31:40 +0900

    Separate SSMCOMDataReceivedEventArgs.

--- WebSocketGaugeServer/ECUSensorCommunication/SSM/SSMCOM.cs
+++ WebSocketGaugeServer/ECUSensorCommunication/SSM/SSMCOM.cs
@@ -231,10 +231,4 @@ namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.SSM
             }
         }
     }
-
-    public class SSMCOMDataReceivedEventArgs : EventArgs
-    {
-	    public bool Slow_read_flag { get; set; }
-	    public List<SSMParameterCode> Received_Parameter_Code { get; set; }
-	}
 }
