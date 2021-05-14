02a8824970cddbedc5781ab18f58fa91830e176a
sz2 <sugiura.k.nigo@gmail.com>
2021-05-15 00:21:11 +0900

    Separate SSM content classes.

---
+++ WebSocketGaugeServer/ECUSensorCommunication/SSM/SSMSwitchContent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.SSM
+{
+    public class SSMSwitchContent
+    {
+        private SSMNumericContent _master_content;
+        private int _bit_index;
+
+        public SSMSwitchContent(SSMNumericContent master_content, int bit_index)
+        {
+            _master_content = master_content;
+            _bit_index = bit_index;
+        }
+
+        public int BitIndex {get => _bit_index; }
+        public SSMNumericContent MasterNumericContent { get => _master_content; }
+        public bool Value
+        {
+            get
+            {
+                return (_master_content.RawValue & 0x01<<_bit_index)>>_bit_index == 1; 
+            }
+        }
+    }
+}
---
+++ WebSocketGaugeServer/ECUSensorCommunication/SSM/VirtualSSMCOM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+
+namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.SSM
+{
+    public class VirtualSSMCOM : SSMCOMBase
+    {
+        private readonly ILogger logger;
+        //コンストラクタ
+        public VirtualSSMCOM(ILoggerFactory logger) : base(logger)
+        {
+            this.logger = logger.CreateLogger<VirtualSSMCOM>();
+        }
+
+        public void SetRawValue(SSMParameterCode code, UInt32 rawValue)
+        {
+            content_table[code].RawValue = rawValue;
+        }
+
+        public void SetSwitch(SSMSwitchCode code, bool value)
+        {
+            var masterNumericCode = content_table[code].
+        }
+
+        protected override void communicate_main(bool slow_read)
+        {
+            var query_SSM_code_list = new List<SSMParameterCode>();
+            foreach (SSMParameterCode code in Enum.GetValues(typeof(SSMParameterCode)))
+            {
+                if (slow_read)
+                {
+                    if (content_table[code].SlowReadEnable)
+                        query_SSM_code_list.Add(code);
+                }
+                else
+                {
+                    if (content_table[code].FastReadEnable)
+                        query_SSM_code_list.Add(code);
+                }
+            }
+                
+            //Invoke SSMDatareceived event
+            SSMCOMDataReceivedEventArgs ssm_received_eventargs = new SSMCOMDataReceivedEventArgs();
+            ssm_received_eventargs.Slow_read_flag = slow_read;
+            ssm_received_eventargs.Received_Parameter_Code = new List<SSMParameterCode>(query_SSM_code_list);
+            OnSSMDataReceived(this,ssm_received_eventargs);
+        }
+   }
+}
