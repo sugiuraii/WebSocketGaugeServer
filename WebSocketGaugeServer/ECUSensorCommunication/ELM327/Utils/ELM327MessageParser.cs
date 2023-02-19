f03dffe1abaa8925750272908f9c36928167510f
sz2 <sugiura.k.nigo@gmail.com>
2023-02-19 17:38:08 +0900

    Fix bugs of ELM327MessageParser.

--- WebSocketGaugeServer/ECUSensorCommunication/ELM327/Utils/ELM327MessageParser.cs
+++ WebSocketGaugeServer/ECUSensorCommunication/ELM327/Utils/ELM327MessageParser.cs
@@ -18,28 +18,30 @@ namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.ELM327.Utils
 
         public ELM327OutMessageParseResult parse(string elm327outStr) 
         {
-            if(elm327outStr.Contains(":")) // Multi frame message
+            string filteredElm327outStr = elm327outStr.Replace("\n","").Replace(" ","");
+            if(filteredElm327outStr.Contains(":")) // Multi frame message
             {
-                string convertedMultilineStr = convertMultiLineMessage(elm327outStr);
+                string convertedMultilineStr = convertMultiLineMessage(filteredElm327outStr);
                 return parseSingleLine(convertedMultilineStr);
             }
             else // Single frame message
-                return parseSingleLine(elm327outStr);
+                return parseSingleLine(filteredElm327outStr);
         }
 
-        private ELM327OutMessageParseResult parseSingleLine(string elm327outStr)
+        private ELM327OutMessageParseResult parseSingleLine(string lineStr)
         {
-            byte modeCode = Byte.Parse(elm327outStr.Substring(0,2));
+            string lineStrFiltered = lineStr.Replace("\r","");
+            byte modeCode = Convert.ToByte(lineStrFiltered.Substring(0,2), 16);
             var valueStrMap = new Dictionary<OBDIIParameterCode, string>();
 
             int ofst = 2;
-            while(ofst < elm327outStr.Length)
+            while(ofst < lineStrFiltered.Length)
             {
-                byte pid = Byte.Parse(elm327outStr.Substring(ofst, 2));
+                byte pid = Convert.ToByte(lineStrFiltered.Substring(ofst, 2), 16);
                 ofst += 2;
                 var code = pidToParamCodeReverseMap[pid];
                 int byteLength = content_table[code].ReturnByteLength;
-                string valueStr = elm327outStr.Substring(ofst, byteLength*2);
+                string valueStr = lineStrFiltered.Substring(ofst, byteLength*2);
                 ofst += byteLength*2;
                 valueStrMap.Add(code, valueStr);
             }
@@ -55,6 +57,8 @@ namespace SZ2.WebSocketGaugeServer.ECUSensorCommunication.ELM327.Utils
             for(int line = 1; line < separatedLineMsg.Length; line++)
             {
                 string lineStr = separatedLineMsg[line];
+                if(lineStr.Length == 0)
+                    continue;
                 string contentStr = lineStr.Split(":")[1];
                 convertedStr += contentStr;
             }
