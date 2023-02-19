5a38526e04b8ca1f724752d488690e43f655df43
sz2 <sugiura.k.nigo@gmail.com>
2023-02-19 18:05:26 +0900

    Add elm327 batch query field to setting file.

--- WebSocketGaugeServer/WebSocketServer/Service/ELM327COMService.cs
+++ WebSocketGaugeServer/WebSocketServer/Service/ELM327COMService.cs
@@ -85,13 +85,14 @@ namespace SZ2.WebSocketGaugeServer.WebSocketServer.Service
                 var elm327AdaptiveTimingMode = serviceSetting["elm327AdaptiveTimingControl"];
                 var elm327Timeout = serviceSetting["elm327Timeout"];
                 var elm327Header = serviceSetting["elm327HeaderBytes"];
+                var elm327PIDBatchQueryCount = int.Parse(serviceSetting["elm327BatchPIDQueryCount"]);
                 logger.LogInformation("ELM327COM COMPort is set to: " + comportName);
 
                 ELM327COM elm327COM;
                 if (elm327ProtocolMode == null || elm327AdaptiveTimingMode == null || elm327Timeout == null)
                     elm327COM = new ELM327COM(loggerFactory, comportName);
                 else
-                    elm327COM = new ELM327COM(loggerFactory, comportName, elm327ProtocolMode, Int32.Parse(elm327AdaptiveTimingMode), Int32.Parse(elm327Timeout), elm327Header);
+                    elm327COM = new ELM327COM(loggerFactory, comportName, elm327ProtocolMode, Int32.Parse(elm327AdaptiveTimingMode), Int32.Parse(elm327Timeout), elm327Header, elm327PIDBatchQueryCount);
 
                 elm327COM.overrideDefaultBaudRate(baudRate);
 
