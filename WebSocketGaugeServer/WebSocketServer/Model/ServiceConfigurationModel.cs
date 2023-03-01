d18cefe4d9a41665ca19d3f221137e6e42482f22
sz2 <sugiura.k.nigo@gmail.com>
2023-03-01 12:21:58 +0900

    Add virtual wait time field to ServiceControlModel.

--- WebSocketGaugeServer/WebSocketServer/Model/ServiceConfigurationModel.cs
+++ WebSocketGaugeServer/WebSocketServer/Model/ServiceConfigurationModel.cs
@@ -20,6 +20,11 @@ namespace SZ2.WebSocketGaugeServer.WebSocketServer.Model
         public bool ArduinoVirtualCOMEnabled {get => Boolean.Parse(ArduinoServiceConfig.GetSection("virtualecu")["enabled"]);}
         public bool DefiVirtualCOMEnabled {get => Boolean.Parse(DefiServiceConfig.GetSection("virtualecu")["enabled"]);}
 
+        public int ELM327VirtualCOMWait {get => int.Parse(ELM327ServiceConfig.GetSection("virtualecu")["waitmsec"]);}
+        public int SSMVirtualCOMWait {get => int.Parse(SSMServiceConfig.GetSection("virtualecu")["waitmsec"]);}
+        public int ArduinoVirtualCOMWait {get => int.Parse(ArduinoServiceConfig.GetSection("virtualecu")["waitmsec"]);}
+        public int DefiVirtualCOMWait {get => int.Parse(DefiServiceConfig.GetSection("virtualecu")["waitmsec"]);}
+
         public bool ELM327COMEnabled {get => Boolean.Parse(ELM327ServiceConfig["enabled"]);}
         public bool SSMCOMEnabled {get => Boolean.Parse(SSMServiceConfig["enabled"]);}
         public bool ArduinoCOMEnabled {get => Boolean.Parse(ArduinoServiceConfig["enabled"]);}
