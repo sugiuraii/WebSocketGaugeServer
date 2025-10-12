a117fd3c2d9392aa27decf3f35662d198e46d977
sz2 <sugiura.k.nigo@gmail.com>
2025-10-12 17:05:19 +0900

    Implementtest code of KeepWakeTimer.

--- WebSocketGaugeServer/WebSocketServer/Middleware/ELM327WebSocketMiddleware.cs
+++ WebSocketGaugeServer/WebSocketServer/Middleware/ELM327WebSocketMiddleware.cs
@@ -39,10 +39,13 @@ namespace SZ2.WebSocketGaugeServer.WebSocketServer.Middleware
             var sessionParam = await service.GetSessionParamAsync(connectionID);
             logger.LogInformation("Session is connected from : " + destAddress.ToString());
 
+            var keepWakeDmyMsgTimer = new KeepAliveDMYMsgTimer(webSocket, 60);
+            keepWakeDmyMsgTimer.Start();
             while (webSocket.State == WebSocketState.Open)
             {
                 await processReceivedMessage(webSocket, service, sessionParam, destAddress, ct);
             }
+            keepWakeDmyMsgTimer.Stop();
             await service.RemoveWebSocketAsync(connectionID);
             if (webSocket.State == WebSocketState.CloseReceived || webSocket.State == WebSocketState.CloseSent)
             {
